f41a8f34d7c32e0381549a3442c544f421ed65e9
otottkovi <otottkoviwin@gmail.com>
2017-05-06 18:27:02 +0200

    beta
    aaa

---
+++ raceEvent.java
@@ -0,0 +1,54 @@
+﻿package hu.ottsoft.racer;
+
+/**
+ * Created by otott on 17/04/2017.
+ */
+
+class raceEvent {
+        int Playerscore;
+    R.string direction;
+    boolean havenitro;
+        int bonus;
+        private int playerSpeed;
+        int playerPosition;
+    public int timetaken;
+    boolean isinfirstplace;
+int fastesttime;
+        boolean finish;
+    int coins
+        havenitro=false;
+        bonus=coins+15;
+        playerSpeed=0;
+        coins=0;
+      for(int laps;laps=1;laps<4;1++){
+
+    }
+    
+    }
+        while(havenitro=true){
+        playerSpeed*2;
+        if (havenitro=false) {
+            break;
+        }
+    }
+        if(timetaken < fastesttime){
+        fastesttime=timetaken;
+    }
+
+
+        while (finish=true){
+        switch(rewards){
+            case "next":
+
+                break;
+            default:
+
+                break;
+        }
+    }
+                    if(isinfirstplace=true){
+        bonus*5;
+
+       }
+}
+}
---
+++ welcome.java
@@ -0,0 +1,13 @@
+package hu.ottsoft.racer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
